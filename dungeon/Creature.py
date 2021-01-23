383cc6c8319c6c4f48bb7b049c74f2d6a9c9c406
Rob Hayes <rob@kdei.co.uk>
2021-01-23 18:50:24 +0000

    load world from data files

---
+++ dungeon/Creature.py
@@ -0,0 +1,7 @@
+class Creature:
+
+    def __init__(self, identifier, name, skill, stamina):
+        self.stamina = stamina
+        self.skill = skill
+        self.name = name
+        self.identifier = identifier
---
+++ dungeon/World.py
@@ -0,0 +1,26 @@
+import json
+
+from dungeon.Creature import Creature
+from dungeon.Location import Location
+
+
+class World:
+
+    def __init__(self):
+        self.locations = {}
+        self.creatures = {}
+        with open("data/world.json") as file:
+            world_dict = json.load(file)
+            for loc in world_dict['locations']:
+                self.locations[loc['identifier']] = Location(**loc)
+            for critter in world_dict['creatures']:
+                self.creatures[critter['identifier']] = Creature(**critter)
+
+    def get_location(self, identifier):
+        return self.locations.get(identifier, None)
+
+    def get_next_location(self, identifier):
+        return self.locations.get(self.locations[identifier].next_location_identifier, None)
+
+    def get_creature(self, identifier):
+        return self.creatures.get(identifier, None)
